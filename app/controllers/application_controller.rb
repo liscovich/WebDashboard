c6d64a80b2d61ff38f3dfa5c4dd3df0a3f3cc916
Oleg Keene <ol.keene@gmail.com>
2012-01-31 20:40:53 +0200

    autoredirect to requested path after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :store_location
+
   helper_method :current_user, :logged_in?
 
   protected
 
   def researcher_required
     redirect_to new_user_session_path, :error => "You must be a researcher!" unless current_user.try(:researcher?)
+  end
+
+  def store_location
+    # If devise model is not User, then replace :user_return_to with :{your devise model}_return_to
+    session[:user_return_to] = request.url unless ["devise/sessions", 'registrations', 'authentications'].include?(params[:controller])
+  end
+
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || stored_location_for(resource) || root_path
   end
 
   def helpers
