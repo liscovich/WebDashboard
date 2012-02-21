cf91c076bf33bf8b874853c6fb1abbf2bc90eb3d
Oleg Keene <ol.keene@gmail.com>
2012-02-21 17:43:05 +0200

    bug fixed in auth type detection

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < Devise::RegistrationsController
   def new
-    session[:auth_type] = request.path =~ /#{DOMAINS[:researcher]}/ || request.subdomains.include?(DOMAINS[:researcher]) ? 'player' : 'researcher'
+    session[:auth_type] = request.path =~ /#{DOMAINS[:researcher]}/ || request.subdomains.include?(DOMAINS[:researcher]) ? 'researcher' : 'player'
     super
   end
 end
