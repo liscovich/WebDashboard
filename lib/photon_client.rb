451a63feb1c04399ca18cd39c04771bb43a7cc19
Sergey Andreyev <san@jetthoughts.com>
2012-04-27 14:35:09 +0300

    Add publishing messages for database worker to photon client

--- lib/photon_client.rb
+++ lib/photon_client.rb
@@ -1,6 +1,7 @@
 require 'eventmachine'
 require 'faye'
 require 'faye/redis'
+require File.expand_path('../websocket/redis.rb', __FILE__)
 DIRNAME = File.dirname(File.expand_path(__FILE__))
 module Photon
   autoload :Socket, "#{DIRNAME}/photon/socket"
@@ -8,10 +9,11 @@ module Photon
   autoload :Client, "#{DIRNAME}/photon/client.rb"
 end
 
-EM.run {
+EM.run do
   client = Photon::Client.new
 
-  redis = Faye::Redis.create(Faye::Engine::Proxy.new({}), {host: 'localhost', namespace: 'server_'})
+  redis_server = Faye::Redis.create(Faye::Engine::Proxy.new({}), {host: 'localhost', namespace: 'server_'})
+  redis_database = WebSocket::Redis.create({host: 'localhost', namespace: 'database_'})
 
   client.connect('localhost', {port: 9090})
 
@@ -20,12 +22,13 @@ EM.run {
   end
 
   client.add_custom_event_listener 123 do |data|
-    redis.publish data, "/game#{message['game_id'] || ''}"
+    data['channel'] = '/game'
+    redis_server.publish(data, [])
+    redis_database.publish(data)
   end
 
   client.add_event_listener 'join' do |data|
     client.raise_event(123, {message: 'test'})
   end
 
-}
-
+end
